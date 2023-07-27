77a9ed059a89faf033f9c0579e0fe50273499d6f
anthonydhudson <ahudson@icstars.org>
2023-07-27 12:48:05 -0500

    enteringcode

---
+++ script.js
@@ -0,0 +1,134 @@
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    {
+      text: 'catalog',
+      href: '#',
+      subLinks: [
+        { text: 'all', href: '/catalog/all' },
+        { text: 'top selling', href: '/catalog/top' },
+        { text: 'search', href: '/catalog/search' },
+      ],
+    },
+    {
+      text: 'orders',
+      href: '#',
+      subLinks: [
+        { text: 'new', href: '/orders/new' },
+        { text: 'pending', href: '/orders/pending' },
+        { text: 'history', href: '/orders/history' },
+      ],
+    },
+    {
+      text: 'account',
+      href: '#',
+      subLinks: [
+        { text: 'profile', href: '/account/profile' },
+        { text: 'sign out', href: '/account/signout' },
+      ],
+    },
+  ];
+  
+  // Task 1.0
+  var mainEl = document.querySelector('main');
+  
+  // Task 1.1
+  mainEl.style.backgroundColor = 'var(--main-bg)';
+  
+  // Task 1.2
+  mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+  
+  // Task 1.3
+  mainEl.classList.add('flex-ctr');
+  
+  // Task 2.0
+  var topMenuEl = document.getElementById('top-menu');
+  
+  // Task 2.1
+  topMenuEl.style.height = '100%';
+  
+  // Task 2.2
+  topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+  
+  // Task 2.3
+  topMenuEl.classList.add('flex-around');
+  
+  // Task 3.0
+  for (var link of menuLinks) {
+    // Task 3.1
+    var aElement = document.createElement('a');
+    aElement.href = link.href;
+    aElement.textContent = link.text;
+    topMenuEl.appendChild(aElement);
+  }
+  
+  // Task 4.0
+  var subMenuEl = document.getElementById('sub-menu');
+  
+  // Task 4.1
+  subMenuEl.style.height = '100%';
+  
+  // Task 4.2
+  subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+  
+  // Task 4.3
+  subMenuEl.classList.add('flex-around');
+  
+  // Task 4.4
+  subMenuEl.style.position = 'absolute';
+  
+  // Task 4.5
+  subMenuEl.style.top = '0';
+  
+  // Task 5.1
+  var topMenuLinks = topMenuEl.querySelectorAll('a');
+  var showingSubMenu = false;
+  
+  // Task 5.2
+  topMenuEl.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.tagName !== 'A') return;
+    console.log(event.target.textContent);
+  });
+  
+  // Task 5.3, 5.4, 5.5, 5.6, 5.7
+  topMenuEl.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.tagName !== 'A') return;
+    topMenuLinks.forEach(function (link) {
+      link.classList.remove('active');
+    });
+    event.target.classList.add('active');
+    showingSubMenu = !!menuLinks.find((link) => link.text === event.target.textContent)
+      .subLinks;
+    if (showingSubMenu) {
+      buildSubMenu(
+        menuLinks.find((link) => link.text === event.target.textContent).subLinks
+      );
+      subMenuEl.style.top = '100%';
+    } else {
+      subMenuEl.style.top = '0';
+    }
+  });
+  
+  // Task 6.2, 6.3
+  subMenuEl.addEventListener('click', function (event) {
+    event.preventDefault();
+    showingSubMenu = false;
+    subMenuEl.style.top = '0';
+    topMenuLinks.forEach(function (link) {
+      link.classList.remove('active');
+    });
+    mainEl.innerHTML = `<h1>${event.target.textContent}</h1>`;
+  });
+  
+  // Task 6.4
+  function buildSubMenu(subLinks) {
+    subMenuEl.innerHTML = '';
+    subLinks.forEach(function (link) {
+      var aElement = document.createElement('a');
+      aElement.href = link.href;
+      aElement.textContent = link.text;
+      subMenuEl.appendChild(aElement);
+    });
+  }
+  
